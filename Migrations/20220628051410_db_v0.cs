71b6bfac326cf4dd3522541d8c45e89ecb23a037
Divu2000 <kaurdivneet2000@gmail.com>
2022-07-06 06:33:09 +0000

    init

---
+++ Migrations/20220628051410_db_v0.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DailySharePrice.Migrations
+{
+    public partial class db_v0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
